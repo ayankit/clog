7d3b11317f2336124f58ac4ba78b22d3205b3cbd
Ankit <ayankit@icloud.com>
2026-02-19 21:31:40 +0530

    Add example

---
+++ clog_test.go
@@ -0,0 +1,28 @@
+package clog_test
+
+import (
+	"errors"
+	"log/slog"
+	"time"
+
+	"github.com/ayankit/clog"
+)
+
+func Example() {
+	clog.Init(slog.LevelDebug)
+
+	clog.Info("Starting server", "addr", ":8080", "env", "production")
+	clog.Debug("Connected to DB", "db", "myapp", "host", "localhost:5432")
+	clog.Warn("Slow request", "method", "GET", "path", "/users", "duration", 497*time.Millisecond)
+	clog.Error("DB connection lost", clog.Err(errors.New("connection reset")), "db", "myapp")
+}
+
+// Create a new logger that writes to given log file
+func Example_withLogToFile() {
+	clog.Init(slog.LevelDebug, "/path/to/log/file")
+
+	clog.Info("Starting server", "addr", ":8080", "env", "production")
+	clog.Debug("Connected to DB", "db", "myapp", "host", "localhost:5432")
+	clog.Warn("Slow request", "method", "GET", "path", "/users", "duration", 497*time.Millisecond)
+	clog.Error("DB connection lost", clog.Err(errors.New("connection reset")), "db", "myapp")
+}
